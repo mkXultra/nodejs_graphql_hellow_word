0c37f8f667e9db5324ca25cf792e9811b977ce55
mkXultra <electrincgoat@gmail.com>
2021-11-20 20:01:30 +0900

    update

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    hello2(name:String): hello2
+  }
+  type hello2 {
+    name: String
+    message: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  hello2: (arg) => {
+  console.log("arg", arg)
+    return {
+      'name': 'this is name',
+      'message': 'this is message',
+    };
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
